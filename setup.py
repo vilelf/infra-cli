a22613ea75d632a1393578711cc58fcdc9653e9e
vilelf <vilelf.dev@gmail.com>
2020-10-02 01:37:17 -0300

    :heavy_plus_sign: adicionando requests, pytest e requests-mock

--- setup.py
+++ setup.py
@@ -5,11 +5,12 @@ setup(
     name='infra-cli',
     version='0.1',
     install_requires=[
-        'click'
+        'click',
+        'requests'
     ],
     entry_points='''
         [console_scripts]
-        infra-cli=main:main
+        infra-cli=main:cli
     ''',
     author='vilelf'
 )
